60382e70a621036d7820335dbe6b3b5bc85f24c4
phobus <immalloc@gmail.com>
2015-11-28 15:05:09 +0100

    Grouping arrays

--- src/js/GData.js
+++ src/js/GData.js
@@ -1,11 +1,34 @@
 (function(window, document, undefined) {
   'use strict';
 
+  /**
+   *  Grouping arrays
+   *  http://codereview.stackexchange.com/questions/37028/grouping-elements-in-array-by-multiple-properties
+   */
   var GData = function() {}
   window['GData'] = GData;
 
   GData.prototype = {
-    
+    arrayFromObject: function(obj) {
+      var arr = [];
+      for (var i in obj) {
+        arr.push(obj[i]);
+      }
+      return arr;
+    },
+
+    groupBy: function(list, fn) {
+      var groups = {};
+      for (var i = 0; i < list.length; i++) {
+        var group = JSON.stringify(fn(list[i]));
+        if (group in groups) {
+          groups[group].push(list[i]);
+        } else {
+          groups[group] = [list[i]];
+        }
+      }
+      return this.arrayFromObject(groups);
+    }
   }
 
 })(window, document);
--- test/GData.js
+++ test/GData.js
@@ -1,8 +1,55 @@
 (function(window, document, undefined) {
   'use strict';
 
-  QUnit.test("Testing GData ok", function(assert) {
+  QUnit.module("GData");
+  QUnit.test("GData groupig", function(assert) {
     assert.ok(window.GData, 'GData is ok');
+
+    var gd = Object.create(GData.prototype);
+    var a1_obj = {
+      text: 'testing'
+    };
+    var a1_arr = ['testing'];
+    assert.deepEqual(gd.arrayFromObject(a1_obj), a1_arr, "GData.arrayFromObject {text:'testing'} to ['testing']");
+
+    var a2_data = [{
+      id1: 1,
+      colum1: 1,
+      colum2: 10,
+      colum3: 100
+    }, {
+      id1: 1,
+      colum1: 1,
+      colum2: 10,
+      colum3: 100
+    }, {
+      id1: 2,
+      colum1: 1,
+      colum2: 10,
+      colum3: 100
+    }];
+    var a2_group = [
+      [{
+        "colum1": 1,
+        "colum2": 10,
+        "colum3": 100,
+        "id1": 1
+      }, {
+        "colum1": 1,
+        "colum2": 10,
+        "colum3": 100,
+        "id1": 1
+      }],
+      [{
+        "colum1": 1,
+        "colum2": 10,
+        "colum3": 100,
+        "id1": 2
+      }]
+    ];
+    assert.deepEqual(gd.groupBy(a2_data, function(item) {
+      return item.id1;
+    }), a2_group, "GData.groupBy function(item) {return item.id1;}");
   });
 
 })(window, document);
